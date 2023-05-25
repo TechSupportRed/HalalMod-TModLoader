ec011184e651d88284a93a6825f324d9264cb268
BuildTools <unconfigured@null.spigotmc.org>
2023-05-25 18:33:52 +0400

    globalplayer

---
+++ GlobalPlayer.cs
@@ -0,0 +1,25 @@
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace HalalMod
+{
+	public class GlobalPlayer : ModPlayer
+	{
+        public bool ExplosionVest = false
+        public float stickyDamage = 0f;
+
+        public override void ProcessTriggers(TriggersSet triggersSet)
+        {
+            if (RiptideMod.berkserkKey.JustPressed && ExplosionVest)
+            {
+
+            }
+        }
+        public override void ResetEffects()
+        {
+            stickyDamage = 0f;
+        }
+    }
+}
