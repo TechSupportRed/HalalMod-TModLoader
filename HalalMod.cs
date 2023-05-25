b115ef7d3b1d12071b42761c64d6c484dd183786
TechSupportRed <120854107+TechSupportRed@users.noreply.github.com>
2023-05-25 18:30:20 +0400

    Update HalalMod.cs

--- HalalMod.cs
+++ HalalMod.cs
@@ -3,6 +3,15 @@ using Terraria.ModLoader;
 namespace HalalMod
 {
 	public class HalalMod : Mod
-	{
-	}
+    {
+          //This creates a key that terraria and the controls settings natively know how to handle
+          public static ModHotKey berserkKey;
+
+        //Loads confdig related stuff such as hotkeys for controls settings in UI
+        public override void Load()
+        {
+            berserkKey = RegisterHotKey("Berserk", "r");
+        }
+    }
+}
 }
