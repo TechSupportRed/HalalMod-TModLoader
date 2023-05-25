32be955ef02a482e1faa7b463d801215ac4f8a71
BuildTools <unconfigured@null.spigotmc.org>
2023-05-25 18:49:49 +0400

    keybind

--- GlobalPlayer.cs
+++ GlobalPlayer.cs
@@ -10,13 +10,13 @@ namespace HalalMod
         public bool ExplosionVest = false;
         public float stickyDamage = 0f;
 
-        public override void ProcessTriggers(TriggersSet triggersSet)
-        {
-            if (RiptideMod.berkserkKey.JustPressed && ExplosionVest)
-            {
-
-            }
-        }
+//        public override void ProcessTriggers(TriggersSet triggersSet)
+//        {
+//            if (HalalMod.berkserkKey.JustPressed && ExplosionVest)
+//            {
+//
+//            }
+//        }
         public override void ResetEffects()
         {
             stickyDamage = 0f;
