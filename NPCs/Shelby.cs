215936791c3df813536c0ca8c6650c01ee4b4081
Universeds <121454278+Universeds@users.noreply.github.com>
2023-05-26 16:44:44 +0400

    Update Shelby.cs

--- NPCs/Shelby.cs
+++ NPCs/Shelby.cs
@@ -20,7 +20,6 @@ namespace HalalMod.NPCs
         {
             NPC.townNPC = true;
             NPC.friendly = true;
-            Main.hardMode = true;
             NPC.width = 20;
             NPC.height = 20;
             NPC.aiStyle = 7;
@@ -42,17 +41,9 @@ namespace HalalMod.NPCs
 
         public override bool CanTownNPCSpawn(int numTownNPCs, int money)
         {
-            for (var i = 0; i < 255; i++)
-            {
-                Player player = Main.player[i];
-                foreach (Item item in player.inventory)
-                {
-                    if (item.type == ItemID.WoodenArrow)
-                    {
-                        return true;
-                    }
-                }
-            }
+        if (Main.hardMode) {
+             return true;
+        }
             return false;
         }
 
